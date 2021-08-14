ae1894ae2807cfbeb9506846fcf04f009f02ca96
Junling Ma <junlingm@uvic.ca>
2021-08-13 20:44:04 -0700

    A new implementation of Averager, which is now a subclass of list.

--- averagers.py
+++ averagers.py
@@ -1,34 +1,13 @@
-class OneByOne:
-    def __init__(self, keys):
-        self.keys = keys
-        self.data = {}
-        self.sims = 0
-        for key in self.keys:
-            self.data[key] = None
+class Averager(list):
+    def __init__(self):
+        self.n = 0
 
-    def add_data(self, new_data):  # assume all data has same times and size
-        for key in self.keys:
-            if self.data[key] is None:
-                self.data[key] = new_data[key]
-            else:
-                for point in range(len(self.data[key])):
-                    self.data[point] = ((self.data[key][point] * self.sims) + new_data[key][point]) / (self.sims + 1)
-        self.sims += 1
-
-
-def averager(data):  # takes a list of lists of comparable data as input
-    size = len(data[0])
-    for sim in data:
-        size = max(size, len(sim))
-    reps = len(data)
-    total = [0 for _ in range(size)]
-    for sim in data:
-        for i in range(size):
-            try:
-                total[i] += sim[i]
-            except IndexError:
-                total[i] += sim[-1]
-    for i in range(size):
-        total[i] /= reps
-    return total
-
+    def __iadd__(self, other):  # assume all data has same times and size
+        if len(self) == 0:
+            for v in other:
+                self.append(v)
+        else:
+            for i in range(len(self)):
+                self[i] = (self[i] * self.n + other[i]) / (self.n + 1)
+        self.n += 1
+        return self
--- sample SIR implementation.py
+++ sample SIR implementation.py
@@ -1,10 +1,9 @@
 from simulation import *
 from population import *
-from agent import *
 from transitions import *
 from numpy import random
 from time import time
-# import cProfile
+from averagers import Averager
 
 
 wait_exp = lambda scale: lambda _: random.exponential(scale)
@@ -14,30 +13,38 @@ beta = 0.3
 gamma = 0.2
 I0 = 10
 
-def to_contact(time, sim, agent, to_state):
+def transmissible(time, sim, agent, to_state):
     v = random.random_sample()
     return v < agent[0].state["infectivity"] * agent[1].state["susceptibility"]
 
 def run(times):
     sim = Simulation("test", N)
-    sim.set(Transition(State("I", "infection"), State("R", "infection"), wait_exp(1/gamma)))
-    sim.set(Transition(State("I", "infection") + State("S", "infection"),
-                       State("I", "infection") + State({"infection":"I", "infectivity":1}),
-                       to_change_callback=to_contact))
-    sim.set(Counter(name="S", state=State("S", "infection")))
-    sim.set(Counter(name="I", state=State("I", "infection")))
-    sim.set(Counter(name="R", state=State("R", "infection")))
+    sim.set(Transition(State("I"), State("R"), wait_exp(1/gamma)))
+    sim.set(Transition(State("I") + State("S"),
+                       State("I") + (State("I") & {"infectivity":1}),
+                       to_change_callback=transmissible))
+    sim.set(Counter(name="S", state=State("S")))
+    sim.set(Counter(name="I", state=State("I")))
+    sim.set(Counter(name="R", state=State("R")))
     sim.set(RandomMixing(sim, beta))
-    sim.set(InitFunction(lambda time, agent: State({"infection":"I", "susceptibility":0, "infectivity":1}) \
-        if agent.id < I0 else State({"infection":"S", "susceptibility":1, "infectivity":0})))
+    sim.set(InitFunction(lambda time, agent: State("I") & {"susceptibility":0, "infectivity":1}
+        if agent.id < I0 else State("S") & {"susceptibility":1, "infectivity":0}))
 
     return sim.run(times)
 
 start_time = time()
-# cProfile.run("v = run(range(150))")
-v = run(range(150))
+S = Averager()
+I = Averager()
+R = Averager()
+for i in range(100):
+    v = run(range(150))
+    S += v["S"]
+    I += v["I"]
+    R += v["R"]
+t = v["times"]
 
+end_time = time()
 for i in range(len(v["times"])):
-    print(v["times"][i], v["S"][i], v["I"][i])
+    print(t[i], S[i], I[i], R[i])
 
-print("--- %s seconds ---" % (time() - start_time))
+print("--- %s seconds ---" % (end_time - start_time))
