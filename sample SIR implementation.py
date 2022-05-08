f8100dd26d07b77dd4651ec27d2a3daf1db0f803
Junling Ma <junlingm@uvic.ca>
2022-05-07 17:27:13 -0700

    Implemented tracing delay (T->X) with a rate theta

--- sample SIR implementation.py
+++ sample SIR implementation.py
@@ -6,29 +6,55 @@ from time import time
 from averagers import Averager
 
 
-wait_exp = lambda scale: lambda _: random.exponential(scale)
+wait_exp = lambda rate: lambda _: random.exponential(1 / rate)
 
-N=10000
-beta = 0.3
-gamma = 0.2
-I0 = 10
+N=5000000
+p = 0.3
+beta = 0.4
+gamma = 0.1
+tau = 0.15
+I0 = 20
+theta = 10
 
-def transmissible(time, sim, agent, to_state):
-    v = random.random_sample()
-    return v < agent[0].state["infectivity"] * agent[1].state["susceptibility"]
+runs = 100
+save = True
+
+def transmitted(time, sim, agent, from_state):
+    l = agent[0].state["transmitted"]
+    if agent[1] not in l:
+        l.append(agent[1])
+        agent[1].state["transmitted"].append(agent[0])
+
+def trace(time, sim, agent, from_state):
+    l = agent.state["transmitted"]
+    agent.state["transmitted"] = list()
+    for c in l:
+        if c.state[None] == "I" and random.random_sample() < p:
+            sim.set_state(time, c, State("T"))
+
+init = lambda time, agent: State({"transmitted": []}) & State("I" if agent.id < I0 else "S")
 
 def run(times):
     sim = Simulation("test", N)
-    sim.set(Transition(State("I"), State("R"), wait_exp(1/gamma)))
+    sim.set(Transition(State("I"), State("R"), wait_exp(gamma)))
     sim.set(Transition(State("I") + State("S"),
-                       State("I") + (State("I") & {"infectivity":1}),
-                       to_change_callback=transmissible))
+                       State("I") + State("I"),
+                       changed_callback=transmitted))
+    sim.set(Transition(from_state=State("I"),
+                       to_state=State("T"),
+                       waiting_time=wait_exp(tau)))
+    sim.set(Transition(from_state=State("T"),
+                       to_state=State("X"),
+                       waiting_time=wait_exp(theta),
+                       changed_callback=trace))
+
     sim.set(Counter(name="S", state=State("S")))
     sim.set(Counter(name="I", state=State("I")))
     sim.set(Counter(name="R", state=State("R")))
+    sim.set(Counter(name="T", state=State("T")))
+    sim.set(Counter(name="X", state=State("X")))
     sim.set(RandomMixing(sim, beta))
-    sim.set(InitFunction(lambda time, agent: State("I") & {"susceptibility":0, "infectivity":1}
-        if agent.id < I0 else State("S") & {"susceptibility":1, "infectivity":0}))
+    sim.set(InitFunction(init))
 
     return sim.run(times)
 
@@ -36,15 +62,29 @@ start_time = time()
 S = Averager()
 I = Averager()
 R = Averager()
-for i in range(100):
-    v = run(range(150))
+T = Averager()
+X = Averager()
+for i in range(runs):
+    print(i)
+    v = run(range(250))
     S += v["S"]
     I += v["I"]
     R += v["R"]
+    T += v["T"]
+    X += v["X"]
+
 t = v["times"]
 
 end_time = time()
-for i in range(len(v["times"])):
-    print(t[i], S[i], I[i], R[i])
+
+if save:
+    import csv
+    with open("SIR.p.{p:.1f}.csv".format(p=p), 'w') as csvfile:
+        w = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        w.writerow(['t', "S", "I", "T", "X", "R"])
+        print('t', "S", "I", "T", "X", "R")
+        for i in range(len(v["times"])):
+            w.writerow([t[i], S[i], I[i], T[i], X[i], R[i]])
+            print(t[i], S[i], I[i], T[i], X[i], R[i])
 
 print("--- %s seconds ---" % (end_time - start_time))
